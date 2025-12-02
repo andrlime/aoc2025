aa1f49c9d5a267687e01c13491b21a25bc36c3e0
Andrew Li <andrewli@u.northwestern.edu>
2025-12-02 01:22:50 -0600

    cleanup day2

--- lib/day2/part2.ml
+++ lib/day2/part2.ml
@@ -2,28 +2,28 @@ open! Shared.Util
 open! Shared.Solver
 open! Common
 
+let get_nth_chunk str chunksize n = String.sub str (n * chunksize) chunksize
+
 let is_invalid_subsequence str chunksize =
   let strlen = String.length str in
   let can_evenly_divide_chunksize = strlen mod chunksize = 0 in
+  let numchunks = strlen / chunksize in
   if not can_evenly_divide_chunksize
   then false
-  else (
-    let numchunks = strlen / chunksize in
-    let chunks =
-      ListUtil.range 0 (numchunks - 1)
-      |> List.map (fun c -> String.sub str (c * chunksize) chunksize)
-    in
-    ListUtil.all_elems_in_list_equal chunks)
+  else
+    ListUtil.range 0 (numchunks - 1)
+    |> List.map (get_nth_chunk str chunksize)
+    |> ListUtil.all_elems_in_list_equal
 ;;
 
 let is_invalid_id id =
   let idstr = string_of_int id in
   let idstr_length = String.length idstr in
-  let range_subsequence_length = ListUtil.range 1 (idstr_length - 1) in
+  let chunksize_range = ListUtil.range 1 (idstr_length - 1) in
   List.fold_left
     (fun acc len -> acc || is_invalid_subsequence idstr len)
     false
-    range_subsequence_length
+    chunksize_range
 ;;
 
 let solver input =
