61ff8ae971c398f19e09306cadf29eac3bdb540f
Andrew Li <andrewli@u.northwestern.edu>
2025-12-01 20:07:34 -0600

    refactor main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -2,7 +2,5 @@ let () =
   let argv = Sys.argv in
   if Array.length argv == 1
   then failwith "missing command line arguments"
-  else (
-    let soln_id = Sys.argv.(1) in
-    Shared.Solver.solve (Advent.Registry.get_solution soln_id))
+  else Advent.Registry.get_solution argv.(1) |> Shared.Solver.solve
 ;;
