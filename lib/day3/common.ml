51a2b2b70f16f07bc0b9dada048534d01207d324
Andrew Li <andrewli@u.northwestern.edu>
2025-12-03 03:46:43 -0600

    refactor Day3.Common.Bank into a module

--- lib/day3/common.ml
+++ lib/day3/common.ml
@@ -1,7 +1,19 @@
 open! Shared.Util
 
-type battery = int
-type bank = battery list
+module Bank = struct
+  type battery = int
+  type bank = battery list
+
+  let battery_of_char c = int_of_char c - int_of_char '0'
+
+  let bank_of_string s =
+    if String.length s = 0
+    then None
+    else Some (StringUtil.charlist_of_string s |> List.map battery_of_char)
+  ;;
+end
+
+include Bank
 
 type accumulator =
   { largestdigit : int
@@ -9,10 +21,3 @@ type accumulator =
   }
 
 let initial_state = { largestdigit = -1; largestindex = -1 }
-let battery_of_char c = int_of_char c - int_of_char '0'
-
-let bank_of_string s =
-  if String.length s = 0
-  then None
-  else Some (StringUtil.charlist_of_string s |> List.map battery_of_char)
-;;
