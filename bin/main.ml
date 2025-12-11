049e54321d586735a3f73b232fa89ebfda8caa44
Andrew Li <andrewli@u.northwestern.edu>
2025-12-11 14:20:05 -0600

    change a == to a =

--- bin/main.ml
+++ bin/main.ml
@@ -2,7 +2,7 @@ let () =
   (* NOTE: This reduces runtime by 50% by cutting the probability of gc latency spikes *)
   Gc.set { (Gc.get ()) with minor_heap_size = 128 * 1024 * 1024; space_overhead = 500 };
   let argv = Sys.argv in
-  if Array.length argv == 1
+  if Array.length argv = 1
   then failwith "missing command line arguments"
   else
     let module Soln = (val Advent.Registry.get_solution argv.(1)) in
